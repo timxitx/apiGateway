d48acaa9d527afedc9c8e85b60261e97e796de8b
timxitx <xitianxu36@gmail.com>
2021-10-01 09:12:49 +1000

    initial

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM openjdk:11
+
+EXPOSE 8080
+ 
+ARG JAR_FILE=target/apiGateway-0.0.1-SNAPSHOT.jar
+
+ADD ${JAR_FILE} apiGateway.jar
+
+COPY /src/main/resources/clientCert.jks /
+ 
+ENTRYPOINT ["java", "-jar", "apiGateway.jar"]
+
+HEALTHCHECK --interval=30s --timeout=3s \
+CMD curl -f https://localhost:7070/asctuator/health | grep UP || exit 1
---
+++ src/main/java/com/gateway/apiGateway/controller/MainController.java
@@ -0,0 +1,51 @@
+package com.gateway.apiGateway.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.security.KeyStore;
+
+import javax.net.ssl.SSLContext;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.impl.client.HttpClients;
+
+
+@RestController
+public class MainController {
+	
+	private static final String MS1_TEST_URI = "https://host.docker.internal:7070";
+	private static final String PASSWORD = "password";
+	private static final String CERT_PATH = "/clientCert.jks";
+
+	@RequestMapping("/ms1Test")
+	public String ms1Test() throws Exception {
+		RestTemplate restTemplate = getRestTemplate();
+		RequestEntity<Object> requestEntity = null;
+		ResponseEntity<String> resp = restTemplate.exchange(MS1_TEST_URI, HttpMethod.GET, requestEntity, String.class);
+		return resp.getBody().trim();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public RestTemplate getRestTemplate() throws Exception {
+		RestTemplate restTemplate = new RestTemplate();
+		File file = new File(CERT_PATH);
+		InputStream is = new FileInputStream(file);
+		KeyStore keystore = KeyStore.getInstance("jks");
+		keystore.load(is, PASSWORD.toCharArray());
+		SSLContext sslcontext = SSLContexts.custom().loadTrustMaterial(keystore).loadKeyMaterial(keystore, PASSWORD.toCharArray()).build();
+		HttpClient client = HttpClients.custom().setHostnameVerifier(SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER).setSSLContext(sslcontext).build();
+		restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(client));
+		return restTemplate;
+	}
+}
