dd09ed35b9ffaa8318be288525092e4dfbd2ed7f
timxitx <xitianxu36@gmail.com>
2021-10-01 17:32:55 +1000

    change url and upload function

--- src/main/java/com/gateway/apiGateway/controller/MainController.java
+++ src/main/java/com/gateway/apiGateway/controller/MainController.java
@@ -8,13 +8,21 @@ import java.security.KeyStore;
 
 import javax.net.ssl.SSLContext;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLContexts;
@@ -24,43 +32,49 @@ import org.apache.http.impl.client.HttpClients;
 @RestController
 public class MainController {
 	
-	private static final String URL_PREFIX = "https://CdkSt-LB8A1-3N5786LEPKC5-1750304150.us-east-2.elb.amazonaws.com";
+	private static final String URL_PREFIX = "https://CdkSt-LB8A1-1BMEQ6O054JAR-115364877.us-east-2.elb.amazonaws.com";
 	private static final String MS1_TEST_URI1 = URL_PREFIX+":7070";
 	private static final String MS1_TEST_URI2 = URL_PREFIX+":8080/test";
 	private static final String MS1_TEST_URI3 = URL_PREFIX+":9090/test";
 	private static final String MS1_TEST_URI4 = URL_PREFIX+":7070/upload";
 	private static final String PASSWORD = "password";
 	private static final String CERT_PATH = "/clientCert.jks";
+	RestTemplate restTemplate;
+	RequestEntity<Object> requestEntity = null;
+
+	public MainController() {
+		try {
+			this.restTemplate = getRestTemplate();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
 	@RequestMapping("/ms1Test")
 	public String ms1Test() throws Exception {
-		RestTemplate restTemplate = getRestTemplate();
-		RequestEntity<Object> requestEntity = null;
-		ResponseEntity<String> resp = restTemplate.exchange(MS1_TEST_URI1, HttpMethod.GET, requestEntity, String.class);
+		ResponseEntity<String> resp = this.restTemplate.exchange(MS1_TEST_URI1, HttpMethod.GET, requestEntity, String.class);
 		return resp.getBody().trim();
 	}
 
 	@RequestMapping("/ms2Test")
 	public String ms2Test() throws Exception {
-		RestTemplate restTemplate = getRestTemplate();
-		RequestEntity<Object> requestEntity = null;
-		ResponseEntity<String> resp = restTemplate.exchange(MS1_TEST_URI2, HttpMethod.GET, requestEntity, String.class);
+		ResponseEntity<String> resp = this.restTemplate.exchange(MS1_TEST_URI2, HttpMethod.GET, requestEntity, String.class);
 		return resp.getBody().trim();
 	}
 
 	@RequestMapping("/ms3Test")
 	public String ms3Test() throws Exception {
-		RestTemplate restTemplate = getRestTemplate();
-		RequestEntity<Object> requestEntity = null;
-		ResponseEntity<String> resp = restTemplate.exchange(MS1_TEST_URI3, HttpMethod.GET, requestEntity, String.class);
+		ResponseEntity<String> resp = this.restTemplate.exchange(MS1_TEST_URI3, HttpMethod.GET, requestEntity, String.class);
 		return resp.getBody().trim();
 	}
 
-	@RequestMapping("/upload")
-	public String upload() throws Exception {
-		RestTemplate restTemplate = getRestTemplate();
-		RequestEntity<Object> requestEntity = null;
-		ResponseEntity<String> resp = restTemplate.exchange(MS1_TEST_URI4, HttpMethod.GET, requestEntity, String.class);
+	@PostMapping("/upload")
+	public String upload(@RequestParam(value = "file") MultipartFile file) throws Exception {
+		MultiValueMap<String, Object> parts = new LinkedMultiValueMap<String, Object>();
+  		parts.add("file", new ByteArrayResource(file.getBytes()));
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(parts, headers);
+		ResponseEntity<String> resp = this.restTemplate.exchange(MS1_TEST_URI4, HttpMethod.POST, requestEntity, String.class);
 		return resp.getBody().trim();
 	}
 	
